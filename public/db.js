a3eb67cc4c4c35cd6c1c17f3385c2c2c03eed5a2
Paul Murray <paulmurray85@gmail.com>
2020-11-03 15:28:37 -0800

    created save record function

--- public/db.js
+++ public/db.js
@@ -1,3 +1,18 @@
+let db;
+var request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = function (event) {
+    const db = event.target.result;
+    db.createObjectStore("pending", {autoIncrement: true});
+};
+
+// FUNCTION TO SAVE RECORD OF TRANSACTION
+function saveRecord(record) {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+}
+
+// FUNCTION TO CHECK DATABASE (ONLINE OR OFFLINE) AND CLEAR STORE WHEN COMPLETED
 function checkDatabase() {
     const transaction = db.transaction(["pending"], "readwrite");
     const store = transaction.objectStore("pending");
